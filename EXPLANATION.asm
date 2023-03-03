a02852ad6845e258f497143c2ffd9850590220d5
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-03-03 21:46:57 +0300

    Create EXPLANATION.asm

---
+++ EXPLANATION.asm
@@ -0,0 +1,19 @@
+Dear Lunima users,
+
+We regret to inform you that our Minecraft utility client, Lunima, will be discontinued effective immediately. As the second lead developer of Lunima, I am writing to inform you of the situation.
+
+There have been some internal conflicts within the development team, particularly with the main developer, IKER_luvely. IKER_luvely has been accused of being negligent and uninterested in the development of Lunima, as well as stealing code from other clients without proper credit or permission. As a result, we have made the difficult decision to discontinue Lunima.
+
+In light of this decision, we want to notify you that the GitHub repository for Lunima will be nuked in the next 24 hours. This is due to the possibility of random code being added by a bot, which may contain malicious code that could harm your system.
+
+We advise all users to be cautious and not download any codes or releases from the Lunima GitHub repository, as it could be dangerous. We apologize for any inconvenience this may cause.
+
+We understand that some of you may have questions or concerns regarding this decision. However, we hope that you will understand our decision and continue to support us in our future endeavors.
+
+Finally, we would like to apologize to our users who have supported us throughout this journey. We acknowledge that Lunima was never as popular as other clients, and that it was built using code from other clients without proper credit or permission. We will ensure that this mistake is not repeated in any future developments.
+
+Thank you for your understanding and support.
+
+Sincerely,
+
+[Choroalp] - Second Lead Developer of Lunima
